b6394d72ad7ac7d9522fd29150819e9c69d7f4df
motokitakumi <mtktkm242424@gmail.com>
2020-09-05 21:57:05 +0900

    [Update]

--- Bookers/config/routes.rb
+++ Bookers/config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
+    resources :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   get "home/about" => "homes#about"
